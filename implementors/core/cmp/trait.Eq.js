3de309097f6325e64841c6728d623f8068b9f1e7
dmweis <dmweis@users.noreply.github.com>
2021-09-30 12:23:08 +0000

    deploy: 91568fd15f0fa16bd308e4dcaadca617b97e64bf

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["rplidar_driver"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"rplidar_driver/struct.ScanPoint.html\" title=\"struct rplidar_driver::ScanPoint\">ScanPoint</a>","synthetic":false,"types":["rplidar_driver::prelude::ScanPoint"]}];
+implementors["rplidar_driver"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"rplidar_driver/struct.ScanPoint.html\" title=\"struct rplidar_driver::ScanPoint\">ScanPoint</a>","synthetic":false,"types":["rplidar_driver::prelude::ScanPoint"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
